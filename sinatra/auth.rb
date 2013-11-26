68cdc6aab00f3728108327d6d98e98fd086aaaab
sonnyparlin <sonnyparlin@gmail.com>
2013-11-25 23:12:15 -0500

    auth

---
+++ sinatra/auth.rb
@@ -0,0 +1,49 @@
+require 'sinatra/base'
+require 'sinatra/flash'
+require 'sinatra/extension'
+
+module Sinatra
+  
+  Auth = Sinatra::Extension.new do
+    module Helpers
+      def authorized?
+        session[:admin] 
+      end
+
+      def protected!
+        halt 401,slim(:unauthorized) unless authorized?
+      end
+    end
+
+    def self.registered(app)
+      app.helpers Helpers
+
+      app.enable :sessions
+
+      app.set :username => 'frank',
+              :password => 'sinatra'
+
+      app.get '/login' do(12)
+        slim :login
+      end
+
+      app.post '/login' do
+        if params[:username] == settings.username && params[:password] == settings.password
+          session[:admin] = true
+          flash[:notice] = "You are now logged in as #{settings.username}"
+          redirect to('/songs')
+        else
+          flash[:notice] = "The username or password you entered are incorrect"
+          redirect to('/login')
+        end
+      end
+
+      app.get '/logout' do
+        session[:admin] = nil
+        flash[:notice] = "You have now logged out"
+        redirect to('/')
+      end
+    end
+  end
+  register Auth
+end
